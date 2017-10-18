fd11c0535f820567d6c4ea192a76ba84af156603
Gregory Perry <gregoryperry627@gmail.com>
2017-10-17 20:24:18 -0700

    started dippin boi

--- Assets/Dipping.cs
+++ Assets/Dipping.cs
@@ -3,16 +3,60 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Dipping : MonoBehaviour {
-
+    public float timeToChoose;
+    public float nextTimeItsShorter;
+    private int score;
+    private int highScore;
+    private GameObject[] sauces;
+    private bool readyForSauce;
 	// Use this for initialization
 	void Start () 
 	{
-		
+        score = 0;
+        readyForSauce = true;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		
-	}
+        while(readyForSauce == true)
+        {
+            timeToChoose -= nextTimeItsShorter;
+            newSauce(timeToChoose);
+        }
+        
+    }
+
+    void newSauce(float time)
+    {
+        readyForSauce = false;
+        int randVal = Random.Range(0, sauces.Length - 1);
+        Instantiate(sauces[randVal], Vector3.zero, Quaternion.identity);
+
+        if (sauces[randVal] = sauces[0]) // if szechuan
+        {
+            //check if player opens and dips
+            //give points
+            score+=5;
+        }
+        else
+        {
+            // check if player swipes right or left
+            score++;
+        }
+    }
+
+    void gotItRight()
+    {
+
+    }
+
+    void gotItWrong()
+    {
+        //game over
+        if(score > highScore)
+        {
+            highScore = score;
+        }
+    }
 }
