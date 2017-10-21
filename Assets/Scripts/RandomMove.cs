fdfe6022577c5cd2953afcaa8b986a7b76002f5e
Gregory Perry <gregoryperry627@gmail.com>
2017-10-20 21:11:15 -0700

    menu scene

---
+++ Assets/Scripts/RandomMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMove : MonoBehaviour {
+
+    public Vector3 somewhere;
+	// Use this for initialization
+	void Start () {
+        somewhere.x = Random.Range(-100, 100);
+        somewhere.y = Random.Range(-100, 100);
+        somewhere.z = Random.Range(-100, 100);
+
+        GetComponent<Rigidbody>().AddForce(somewhere*100);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
