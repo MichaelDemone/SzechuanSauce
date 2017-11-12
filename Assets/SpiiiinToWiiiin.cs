503f25cb6b36f08aa8beca5864295630151f44a0
Gregory Perry <gregoryperry627@gmail.com>
2017-11-12 15:17:00 -0800

    THERESABIGFUCKINGNUGGETONMYMENU!!!!

---
+++ Assets/SpiiiinToWiiiin.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiiiinToWiiiin : MonoBehaviour {
+    public GameObject nugget;
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		nugget.GetComponent<Transform>().Rotate(new Vector3(0, 0, 100*Time.deltaTime));
+	}
+}
